c7fe90f0592fd1ce954b864cd0163e06024610d1
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2022-05-20 16:16:00 +0300

    update on manageaccount

--- manageaccount.php
+++ manageaccount.php
@@ -1,77 +1,83 @@
 <?php include("includes/includedFiles.php"); ?>
 <script>
-$(".lds-facebook").hide();
-$("#mainContent").show();
+    $(".lds-facebook").hide();
+    $("#mainContent").show();
 </script>
 
 <?php if ($userRegstatus === "registered") : ?>
 
 
-<div class="pagesection">
+    <div class="pagesection">
 
-    <div class="userCenterSection">
-        <div class="userInfo">
-            <h3>ACCOUNT INFO</h3>
+        <div class="userCenterSection">
 
-            <img src="assets/images/profile-pics/me.jpg" alt="">
+            <div class="userInfo">
 
-            <p class="userlable">USER NAME </P>
-            <P class="uservalue"><?php echo $userLoggedIn->getUsername(); ?></P>
+                <h3>ACCOUNT INFO</h3>
 
-            <p class="userlable">FULL NAME</P>
-            <P class="uservalue"><?php echo $userLoggedIn->getFirstAndLastName(); ?></P>
+                <div class="useraccountinfo">
+                    <img src="assets/images/profile-pics/me.jpg" alt="">
 
-            <p class="userlable">EMAIL </P>
-            <P class="uservalue"><?php echo $userLoggedIn->getEmail(); ?></P>
+                    <div class="accountdetails">
+                        <p class="userlable">USER NAME </P>
+                        <P class="uservalue"><?php echo $userLoggedIn->getUsername(); ?></P>
+
+                        <p class="userlable">FULL NAME</P>
+                        <P class="uservalue"><?php echo $userLoggedIn->getFirstAndLastName(); ?></P>
+
+                        <p class="userlable">EMAIL </P>
+                        <P class="uservalue"><?php echo $userLoggedIn->getEmail(); ?></P>
 
 
+                    </div>
+                </div>
 
-            <div class="UserbuttonItems">
-                <button class="userbuton" onclick="openPage('settings')">Update User Info</button>
-                <button class="userbuton" onclick="logout()">Log Out</button>
+                <div class="UserbuttonItems">
+                    <button class="userbuton" onclick="openPage('settings')">Update User Info</button>
+                    <button class="userbuton" onclick="logout()">Log Out</button>
+                </div>
+
             </div>
+
+
 
         </div>
 
-
-
     </div>
-
-</div>
 
 
 <?php else :  ?>
 
-<script>
-    showPreviewDialog();
-</script>
+    <script>
+        showPreviewDialog();
+    </script>
 
-<div class="dismissable">
+    <div class="dismissable">
 
-    <div class="dismissablecontent">
+        <div class="dismissablecontent">
 
-        <div class="previewimagecontainer">
-            <img src="assets/images/livethumbnails/premium.svg" alt="images">
-        </div>
-
-        <div class="dismissablecontentdesc">
-
-            <div class="dismissablecontenttext">
-                <h4>Mwonyaa Stream</h4>
-                <h2>Manage Account</h2>
-
-                <p>Manage your Account and stay upto date with new Feature Releases on Mwonyaa. Get Monthly Mwonyaa Points and information on your listening history</p>
+            <div class="previewimagecontainer">
+                <img src="assets/images/livethumbnails/premium.svg" alt="images">
             </div>
 
-            <div class="dismissablecontentbutton">
-                <button class="liveshowbtn" onclick="createAccount();">Create Account</button>
+            <div class="dismissablecontentdesc">
+
+                <div class="dismissablecontenttext">
+                    <h4>Mwonyaa Stream</h4>
+                    <h2>Manage Account</h2>
+
+                    <p>Manage your Account and stay upto date with new Feature Releases on Mwonyaa. Get Monthly Mwonyaa Points and information on your listening history</p>
+                </div>
+
+                <div class="dismissablecontentbutton">
+                    <button class="liveshowbtn" onclick="createAccount();">Create Account</button>
+                </div>
+
             </div>
 
         </div>
 
     </div>
-
-</div>
 
 
 <?php endif ?>
@@ -79,6 +85,6 @@ $("#mainContent").show();
 
 
 <script>
-var elmnt = document.getElementById("mainContent");
-elmnt.scrollTop = 0
+    var elmnt = document.getElementById("mainContent");
+    elmnt.scrollTop = 0
 </script>
